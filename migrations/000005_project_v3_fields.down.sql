9d884a2adfff526c8eb7b934ff72290124943fbc
n0r41n <ivano.turi@gmail.com>
2026-07-10 15:28:13 +0200

    feat(project): migration adds v3 fields (category, assignee type, style, private)

---
+++ migrations/000005_project_v3_fields.down.sql
@@ -0,0 +1,6 @@
+ALTER TABLE projects DROP COLUMN category_id;
+ALTER TABLE projects DROP COLUMN assignee_type;
+ALTER TABLE projects DROP COLUMN is_private;
+ALTER TABLE projects DROP COLUMN simplified;
+ALTER TABLE projects DROP COLUMN style;
+ALTER TABLE projects DROP COLUMN url;
